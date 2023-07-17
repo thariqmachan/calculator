d087872df0da8a44fc950f943c2eaa97166244d8
unknown <thariqmohammed660@gmail.com>
2023-07-17 19:31:29 +0530

    calculator

---
+++ calc.js
@@ -0,0 +1,15 @@
+function displayData(n){
+    result.value=result.value+n 
+}
+
+function equalto(){
+    result.value=eval(result.value)
+}
+
+function AllClear(){
+    result.value=""
+}
+
+function backSpace(){
+    result.value=result.value.slice(0,-1)
+}
